dd6805ae261fc32063a0b680ed19551f3823d0ba
h4r4ld-git <luka.chkoidze@student.uclouvain.be>
2022-01-21 13:54:34 +0100

    INDEX

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const https = require('https');
 const fs = require('fs');
 const dotenv = require('dotenv');
 const validator = require('validator')
-const pinSender = require('lib/MailSender')
+const pinSender = require('./lib/MailSender')
 
 const {MongoClient} = require('mongodb');
 const dbURL = "mongodb+srv://h4r4ld:.feEM5*46pXzFM4@surf.hdzzn.mongodb.net/"
