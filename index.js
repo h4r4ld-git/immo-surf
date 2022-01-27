16d7815dfc3b5386872592109969415f4b406c0e
h4r4ld-git <luka.chkoidze@student.uclouvain.be>
2022-01-27 02:02:48 +0100

    MAIN

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get('/', function(req, res) {
       users = client.db("immo-surf").collection("users")
       users.findOne({email: req.session.user.email}, function(err, result){
         if (result){
-          affiches.find({email: req.session.user.email}).toArray(function(err, affs){
+          affiches.find({mail: req.session.user.email}).toArray(function(err, affs){
             res.render('surf.html', {myProfile: profilePage(result, affs)})
           })
         }
